99e20566a3f9da45065ed1fbe9dfadb8607b241a
lenovo <berguzardemirhan@gmail.com>
2023-07-12 10:19:19 +0300

    edit

--- odev2/main.py
+++ odev2/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 data = pd.read_csv(r"C:/Users/lenovo/PycharmProjects/pythonProject/odev2/Iris.csv",)
 
 data.loc[data['species'] == 'Iris-setosa', 'species'] = 'manolya'
-data.loc[data['species'] == 'Iris-versicolor', 'species'] = 'papatya'
+data.loc[data['species'] == 'Iris-versicolor', 'species'] = 'gül'
 data.loc[data['species'] == 'Iris-virginica', 'species'] = 'lale'
 
 data.to_excel(r"C:/Users/lenovo/PycharmProjects/pythonProject/odev2/Dataset.xlsx")
